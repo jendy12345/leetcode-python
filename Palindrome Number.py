94bd9299b0353c0aa706089e155a117a4192c4f7
python practice <40566142+jendy12345@users.noreply.github.com>
2024-01-22 14:42:20 +0800

    Update Palindrome Number.py

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,7 +1,21 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        x=str(x)
-        for i in range(len(x)//2):
-            if x[i]!=x[-i-1]:
+class Solution(object):
+    def __init__(self,x):
+        self.x = x
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x) # 將整數x轉為字串
+        for i in range(len(x)//2):  # 使用迴圈遍歷字串的前半部，由於後半部為對稱，所以不須再檢查一次
+            if x[i] != x[-i-1]: # 檢查正序、到序的兩個位置是否相等，如果都相等，則回傳True，如果沒有則回傳False
                 return False
-        return True 
+        return True    
+        
+
+
+if __name__ =='__main__':
+    x = -141
+    a = Solution(x)
+    result = a.isPalindrome(x)
+    print(result)
